5e3160371da146105b0807c1787c36731f6f95b7
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-10 22:14:14 +0600

    add project router created

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { signin, signup } from '../Controller/userController.js'
+import { addProject } from '../Controller/projectController.js';
 
 const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.post('/addProject', addProject);
 
 export default router; 
