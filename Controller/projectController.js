4d02694f807810e4dbb3ed2adcd64899fd63c529
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-12 12:56:23 +0600

    project validation added

--- Controller/projectController.js
+++ Controller/projectController.js
@@ -2,11 +2,16 @@ import Project from "../model/projectSchema.js";
 
 //add project api
 export const addProject = async (req, res) => {
+    const exist = await Project.findOne({ projectName: req.body.projectName })
     try {
-        const project = req.body;
-        const newProject = new Project(project);
-        await newProject.save();
-        res.status(200).json({ message: project })
+        if (exist) {
+            return res.status(401).json({ message: 'Project already exist' })
+        } else {
+            const project = req.body;
+            const newProject = new Project(project);
+            await newProject.save();
+            res.status(200).json({ message: project })
+        }
     } catch (err) {
         console.log(res.status(500).json({ message: err.message }))
     }
