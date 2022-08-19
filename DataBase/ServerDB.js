225924ac8f8b283c7275804d876dc9697d7c8d2a
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-19 17:22:08 +0600

    updated file

--- DataBase/ServerDB.js
+++ DataBase/ServerDB.js
@@ -5,10 +5,10 @@ const connection = async () => {
     try {
         await mongoose.connect(
             // DB NAJMUL
-            // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhsfr.mongodb.net/itracker`
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhsfr.mongodb.net/itracker`
 
             // DB HAMED
-            `mongodb+srv://iTracker:OtE0nMRyVrzzXkpW@cluster0.dw5trqg.mongodb.net/?retryWrites=true&w=majority`
+            // `mongodb+srv://iTracker:OtE0nMRyVrzzXkpW@cluster0.dw5trqg.mongodb.net/?retryWrites=true&w=majority`
         )
         console.log("DB connected");
     } catch (error) {
